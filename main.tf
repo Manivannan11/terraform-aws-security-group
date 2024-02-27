cac65e5ca16ea90c3f9e4086d3e0e8d214ec5e19
manivannan11 <mallai.mani1112@gmail.com>
2024-02-27 23:26:53 +0530

    adding code

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-# Define the security group resource
-resource "aws_security_group" "example_sg" {
-  name        = var.security_group_name
-  description = var.security_group_description
-  vpc_id      = var.vpc_id
+# # Define the security group resource
+# resource "aws_security_group" "example_sg" {
+#   name        = var.security_group_name
+#   description = var.security_group_description
+#   vpc_id      = var.vpc_id
 
-  // Ingress and egress rules as needed
-}
-
-# resource "aws_instance" "my_instance" {
-#   ami           = "ami-0440d3b780d96b29d" # us-west-2
-#   instance_type = "t2.micro"
+#   // Ingress and egress rules as needed
 # }
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0440d3b780d96b29d" # us-west-2
+  instance_type = "t2.micro"
+}
