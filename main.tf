5396ca630c764888a1910718ce5947b3eef64a88
manivannan11 <mallai.mani1112@gmail.com>
2024-02-27 22:35:42 +0530

    adding vpc id

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = data.aws_vpc.existing_vpc.id
+# resource "aws_security_group" "allow_tls" {
+#   name        = "allow_tls"
+#   description = "Allow TLS inbound traffic and all outbound traffic"
+#   vpc_id      = data.aws_vpc.existing_vpc.id
 
-  tags = {
-    Name = "allow_tls"
-  }
+#   tags = {
+#     Name = "allow_tls"
+#   }
+# }
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0440d3b780d96b29d" # us-west-2
+  instance_type = "t2.micro"
 }
