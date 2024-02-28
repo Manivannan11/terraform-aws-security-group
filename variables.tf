52ae667781ada81604cfac43729334816dc18b8e
manivannan11 <mallai.mani1112@gmail.com>
2024-02-28 15:09:06 +0530

    adding ami id

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "ami_id" {
   type        = string
   description = "AMI ID to be used"
+  default     = null 
 }
 
 variable "env" {
   type        = string
   description = "Environment that app is going to run"
+  default     = null
 }
 
 
